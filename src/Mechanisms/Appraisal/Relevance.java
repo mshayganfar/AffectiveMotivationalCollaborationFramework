226a9da7113a2ebc72969a03449082e6651fdd18
mohammad <mshayganfar@wpi.edu>
2016-02-08 15:18:38 -0500

    Minor changes.

--- src/Mechanisms/Appraisal/Relevance.java
+++ src/Mechanisms/Appraisal/Relevance.java
@@ -24,10 +24,17 @@ public class Relevance extends AppraisalProcesses {
 	
 	private double getEventUtility(Goal eventGoal) { 
 		
-		int beliefPersistence = getBeliefPersistence(getLastRelatedBelief(eventGoal));
-		System.out.println(beliefPersistence);
+		int goalStatus           = getGoalStatus(eventGoal);
+		int beliefPersistence    = getBeliefPersistence(getLastRelatedBelief(eventGoal));
+		int beliefSaliency       = getBeliefSaliency(eventGoal); //Distance between live nodes 
+		double saliencyMagnitude = getSaliencyMagnitude(eventGoal);
 		
-		return 0.0; 
+		if (saliencyMagnitude > 0)
+			return goalStatus*beliefPersistence*Math.pow(beliefSaliency, saliencyMagnitude);
+		else if(saliencyMagnitude == 0)
+			return 0.0;
+		else 
+			return -2.0;
 	}
 	
 	private double getEmotionalThreshold() { 
@@ -45,5 +52,20 @@ public class Relevance extends AppraisalProcesses {
 	private Belief getLastRelatedBelief(Goal goal) {
 		
 		return goal.getBeliefs().get(goal.getBeliefs().size()-1);
+	}
+	
+	private int getBeliefSaliency(Goal goal) {
+		
+		return 0;
+	}
+	
+	private int getGoalStatus(Goal goal) {
+		
+		return 1;
+	}
+	
+	private double getSaliencyMagnitude(Goal goal) {
+		
+		return 1.0;
 	}
 }
