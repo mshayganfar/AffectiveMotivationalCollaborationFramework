99ad5f3dbaccadfe8b0895512b8fc1805a065d6e
mohammad <mshayganfar@wpi.edu>
2016-03-04 16:04:37 -0500

    Worked on goal extraction and Disco integration.

--- src/AppraisalGoalManagement.java
+++ src/AppraisalGoalManagement.java
@@ -2,6 +2,8 @@ import Mechanisms.Appraisal.AppraisalProcesses;
 import Mechanisms.Appraisal.Relevance;
 import MentalState.Belief;
 import MentalState.Goal;
+import edu.wpi.cetask.Plan;
+import edu.wpi.cetask.TaskClass;
 import edu.wpi.cetask.TaskModel;
 import edu.wpi.disco.Agent;
 import edu.wpi.disco.Disco;
@@ -10,9 +12,44 @@ import edu.wpi.disco.User;
 
 public class AppraisalGoalManagement {
 	
-	public static void testAppraisal() { System.out.println("appraisal works!"); }
+	private static Disco disco;
+	private static TaskModel taskModel;
+	
+	public static TaskClass getGoal(Plan plan) {
+		
+		while (!plan.getType().getNamespace().toString().equals(taskModel.toString()))
+			plan = plan.getParent();
+		
+		return plan.getGoal().getType();
+	}
+	
+	public static void doAppraisal() {
+		
+		if (disco.getFocus() != null) {
+			System.out.println(getGoal(disco.getFocus()));
+		} else {
+			System.out.println("No goal in the stack!");
+		}
+	}
 	
 	public static void main(String[] args) {
+		
+		Interaction interaction = new Interaction(new Agent("agent"), new User("user"),
+				  args.length > 0 && args[0].length() > 0 ? args[0] : null);
+
+		interaction.getExternal().setEval(true);
+		interaction.start(true);
+
+		disco = interaction.getDisco();
+
+		System.out.println(disco);
+
+		taskModel = disco.load("/TaskModel/Sandwich.xml");
+		disco.load("/TaskModel/Events.xml");
+		
+//		interaction.getConsole().source("test/events.txt");
+//		interaction.getConsole().test("test/Console.test");
+//		interaction.getConsole().step("test/Console.test");
 		
 //		Goal eventGoal1 = new Goal(null);
 //		Belief belief1 = new Belief(eventGoal1);
@@ -22,24 +59,12 @@ public class AppraisalGoalManagement {
 //		Goal eventGoal2 = new Goal(null);
 //		Belief belief4 = new Belief(eventGoal2);
 //		Belief belief5 = new Belief(eventGoal2);
-//		
+		
 //		Relevance relevance = new Relevance(args);
 //		relevance.isEventRelevant(eventGoal1);
 //		
 //		relevance.isEventRelevant(eventGoal2);
 		
-		Interaction interaciton = new Interaction(new Agent("agent"), new User("user"),
-				  args.length > 0 && args[0].length() > 0 ? args[0] : null);
-
-		interaciton.start(true);
-
-		Disco disco = interaciton.getDisco();
-
-		System.out.println(disco);
-
-		TaskModel taskModel = disco.load("/TaskModel/Sandwich.xml");
-//		disco.load("/TaskModel/Events.xml");
-
 		
 //		Task task = taskModel.getTaskClass("d").newInstance();
 //
--- src/Mechanisms/Appraisal/AppraisalProcesses.java
+++ src/Mechanisms/Appraisal/AppraisalProcesses.java
@@ -16,6 +16,4 @@ public class AppraisalProcesses{
 	
 	// The return value can be computed according to the emotional status of the human.
 	protected double getHumanEmotionalThreshold() { return 0.0; }
-	
-	public static void testAppraisal() { System.out.println("appraisal works!"); }
 }
