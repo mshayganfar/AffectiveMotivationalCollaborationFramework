6e882649f5c10f78d7e08d631d0bd8e14ae52361
mshayganfar <mshayganfar@wpi.edu>
2016-03-31 16:39:02 -0400

    Made all services as members of a class.

--- catkin_ws/src/amc_framework/src/main.cpp
+++ catkin_ws/src/amc_framework/src/main.cpp
@@ -1,7 +1,5 @@
 #include "kuka-move.hpp"
 
-MoveKuka kukaObj;
-
 void UpdateRobotPose(const sensor_msgs::JointState::ConstPtr& msg) {
 
  // make sure we're not reading the base wheel joint values
@@ -19,47 +17,10 @@ void ArmResultListener(const control_msgs::FollowJointTrajectoryActionResult::Co
   }
 }
 
-bool GoHome(std_srvs::Empty::Request  &req,
-            std_srvs::Empty::Response &res)
-{
-  MoveArmHome();
-  return true;
-}
-
-bool GoRight(std_srvs::Empty::Request  &req,
-             std_srvs::Empty::Response &res)
-{
-  MoveArmRight();
-  return true;
-}
-
-bool GoLeft(std_srvs::Empty::Request  &req,
-             std_srvs::Empty::Response &res)
-{
-  MoveArmLeft();
-  return true;
-}
-
-bool OpenGripper(std_srvs::Empty::Request  &req,
-             std_srvs::Empty::Response &res)
-{
-  double val = 0.0115;
-  std::cout << "Opening gripper..." << std::endl;
-  PublishGripperJointCommand(val);
-  WaitForGripper(val);
-  return true;
-}
-
-bool CloseGripper(std_srvs::Empty::Request  &req,
-             std_srvs::Empty::Response &res) {
-  double val = 0.0;
-  PublishGripperJointCommand(val);
-  WaitForGripper(val);
-  return true;
-}
-
 int main(int argc, char** argv) {
- 
+
+	MoveKuka kukaObj;
+
   q.resize(5);
   qArmHome.resize(5);
   qArmRight.resize(5);
@@ -81,11 +42,11 @@ int main(int argc, char** argv) {
 
   gripperPositionPublisher = nh.advertise<brics_actuator::JointPositions > ("arm_1/gripper_controller/position_command", 1);
 
-  ros::ServiceServer serviceGoHome  = nh.advertiseService("go_home", GoHome);
-  ros::ServiceServer serviceGoRight = nh.advertiseService("go_right", GoRight);
-  ros::ServiceServer serviceGoLeft  = nh.advertiseService("go_left", GoLeft);
-  ros::ServiceServer serviceOpenGripper  = nh.advertiseService("open_gripper", OpenGripper);
-  ros::ServiceServer serviceCloseGripper = nh.advertiseService("close_gripper", CloseGripper);
+  ros::ServiceServer serviceGoHome  = nh.advertiseService("go_home", &MoveKuka::GoHome, &kukaObj);
+  ros::ServiceServer serviceGoRight = nh.advertiseService("go_right", &MoveKuka::GoRight, &kukaObj);
+  ros::ServiceServer serviceGoLeft  = nh.advertiseService("go_left", &MoveKuka::GoLeft, &kukaObj);
+  ros::ServiceServer serviceOpenGripper  = nh.advertiseService("open_gripper", &MoveKuka::OpenGripper, &kukaObj);
+  ros::ServiceServer serviceCloseGripper = nh.advertiseService("close_gripper", &MoveKuka::CloseGripper, &kukaObj);
 
   ros::MultiThreadedSpinner spinner(4); // Use 4 threads
   spinner.spin(); // spin() will not return until the node has been shutdown
