74325fef349ca662ca414477b98aa1db32ee56b0
mohammad <mshayganfar@wpi.edu>
2016-04-29 10:52:46 -0400

    Made some changes to the DiscoActionsWrapper. + Removed previous BUG.

--- src/Mechanisms/Appraisal/DiscoActionsWrapper.java
+++ src/Mechanisms/Appraisal/DiscoActionsWrapper.java
@@ -2,6 +2,7 @@ package Mechanisms.Appraisal;
 
 import Mechanisms.Collaboration.Collaboration;
 import MentalState.Goal;
+import edu.wpi.cetask.DecompositionClass;
 import edu.wpi.cetask.Plan;
 import edu.wpi.cetask.Task;
 import edu.wpi.disco.lang.Accept;
@@ -19,53 +20,120 @@ public class DiscoActionsWrapper {
 		this.collaboration = collaboration;
 	}
 	
-	void proposeTask(Goal goal, Plan contributes, boolean speaker) {
+	void proposeTaskWho(Goal goal, boolean speaker) {
 		
 		// NOTE: Speaker should be false when the agent is doing coping.
 		
 		Task taskToPropose;
-		Task task = goal.getPlan().getGoal();
+		Plan plan = goal.getPlan();
 		
-		if (task.isPrimitive())
-			taskToPropose = new Propose.Who(collaboration.getDisco(), speaker, task, !speaker);
+		if (plan.isPrimitive()) {
+			taskToPropose = new Propose.Who(collaboration.getDisco(), speaker, plan.getGoal(), !speaker);
+			collaboration.getDisco().getInteraction().occurred(speaker, taskToPropose, plan);
+		}
 		else
-			taskToPropose = new Propose.Should(collaboration.getDisco(), speaker, task);
-		
-		collaboration.getDisco().getInteraction().occurred(speaker, taskToPropose, contributes);
+			throw new IllegalArgumentException("Both of the collaborators are responsible for non-primitives");
 	}
 	
-	void acceptProposedTask(Goal goal, Plan contributes, boolean speaker) {
+	void proposeTaskShould(Goal goal, boolean speaker) {
 		
-		Task task = goal.getPlan().getGoal();
+		Plan plan = goal.getPlan();
 		
-		Utterance acceptance = new Accept(collaboration.getDisco(), speaker, new Propose.Should(collaboration.getDisco(), speaker, task));
-		collaboration.getDisco().getInteraction().occurred(speaker, acceptance, contributes);
+		Task taskToPropose = Propose.Should.newInstance(collaboration.getDisco(), speaker, plan.getGoal());
+		collaboration.getDisco().getInteraction().occurred(speaker, taskToPropose, plan);
+	}
+
+	void proposeTaskWhat(Goal goal, boolean speaker, String slot, Object value) {
+		
+		Plan plan = goal.getPlan();
+		
+		Task taskToPropose = new Propose.What(collaboration.getDisco(), speaker, plan.getGoal(), slot, value);
+		collaboration.getDisco().getInteraction().occurred(speaker, taskToPropose, plan);
 	}
 	
-	void rejectProposedTask(Goal goal, Plan contributes, boolean speaker) {
+	void proposeTaskHow(Goal goal, boolean speaker, DecompositionClass decomp) {
 		
-		Task task = goal.getPlan().getGoal();
+		Task taskToPropose;
+		Plan plan = goal.getPlan();
 		
-		Utterance rejection = new Reject(collaboration.getDisco(), speaker, new Propose.Should(collaboration.getDisco(), speaker, task));
-		collaboration.getDisco().getInteraction().occurred(speaker, rejection, contributes);
+		if (!plan.isPrimitive()) {
+			taskToPropose = new Propose.How(collaboration.getDisco(), speaker, plan, decomp);
+			collaboration.getDisco().getInteraction().occurred(speaker, taskToPropose, plan);
+		}
+		else
+			throw new IllegalArgumentException("Primitives does not need recepies!");
 	}
 	
-	void mentionTask(Goal goal, Plan contributes, boolean speaker) {
+	void acceptProposedTask(Goal goal, boolean speaker) {
 		
-		Task taskToMention = new Mention(collaboration.getDisco(), speaker, goal.getPlan().getGoal());
-		collaboration.getDisco().getInteraction().occurred(speaker, taskToMention, contributes);
+		Plan plan = goal.getPlan();
+		
+		Utterance acceptance = new Accept(collaboration.getDisco(), speaker, Propose.Should.newInstance(collaboration.getDisco(), speaker, plan.getGoal()));
+		collaboration.getDisco().getInteraction().occurred(speaker, acceptance, plan);
 	}
 	
-	void askAboutTask(Goal goal, Plan contributes, boolean speaker) {
+	void rejectProposedTask(Goal goal, boolean speaker) {
 		
-		Task taskToAsk = new Ask.Who(collaboration.getDisco(), speaker, goal.getPlan().getGoal());
-		collaboration.getDisco().getInteraction().occurred(speaker, taskToAsk, contributes);
+		Plan plan = goal.getPlan();
+		
+		Utterance rejection = new Reject(collaboration.getDisco(), speaker, Propose.Should.newInstance(collaboration.getDisco(), speaker, plan.getGoal()));
+		collaboration.getDisco().getInteraction().occurred(speaker, rejection, plan);
 	}
 	
-	void executeTask(Goal goal, Plan contributes, boolean speaker) {
+	void mentionTask(Goal goal, boolean speaker) {
 		
-		if (goal.getPlan().getGoal().isPrimitive())
-			collaboration.getDisco().getInteraction().occurred(speaker, goal.getPlan().getGoal(), contributes);
+		Plan plan = goal.getPlan();
+		
+		Task taskToMention = new Mention(collaboration.getDisco(), speaker, plan.getGoal());
+		collaboration.getDisco().getInteraction().occurred(speaker, taskToMention, plan);
+	}
+	
+	void askAboutTaskWho(Goal goal, boolean speaker) {
+		
+		Plan plan = goal.getPlan();
+		
+		if (plan.isPrimitive()) {
+			Task taskToAsk = new Ask.Who(collaboration.getDisco(), speaker, plan.getGoal());
+			collaboration.getDisco().getInteraction().occurred(speaker, taskToAsk, plan);
+		}
+		else
+			throw new IllegalArgumentException("Both of the collaborators are responsible for non-primitives");
+	}
+	
+	void askAboutTaskShould(Goal goal, boolean speaker) {
+		
+		Plan plan = goal.getPlan();
+		
+		Task taskToAsk = new Ask.Should(collaboration.getDisco(), speaker, plan.getGoal());
+		collaboration.getDisco().getInteraction().occurred(speaker, taskToAsk, plan);
+	}
+	
+	void askAboutTaskWhat(Goal goal, boolean speaker) {
+		
+		Plan plan = goal.getPlan();
+		
+		Task taskToAsk = new Ask.What(collaboration.getDisco(), speaker, (Propose.What)plan.getGoal());
+		collaboration.getDisco().getInteraction().occurred(speaker, taskToAsk, plan);
+	}
+	
+	void askAboutTaskHow(Goal goal, boolean speaker) {
+		
+		Plan plan = goal.getPlan();
+		
+		if (!plan.isPrimitive()) {
+			Task taskToAsk = new Ask.How(collaboration.getDisco(), speaker, plan.getGoal());
+			collaboration.getDisco().getInteraction().occurred(speaker, taskToAsk, plan);
+		}
+		else
+			throw new IllegalArgumentException("Primitives does not need recepies!");
+	}
+
+	void executeTask(Goal goal, boolean actor) {
+		
+		Plan plan = goal.getPlan();
+		
+		if (plan.isPrimitive())
+			collaboration.getDisco().getInteraction().occurred(actor, plan.getGoal(), plan);
 		else
 			throw new IllegalArgumentException("Non-primitive tasks can not be executed!");
 	}
